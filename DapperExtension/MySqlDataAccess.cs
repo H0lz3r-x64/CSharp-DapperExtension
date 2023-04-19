74ef02e97188148e8e7fe64ceba5ccf343a7e921
Luca Holzer <luca.holzer@knapp.com>
2023-04-19 14:19:58 +0200

    Added CheckConnection to verify if the connection can be established when creating the MySqlDataAccess object

--- DapperExtension/MySqlDataAccess.cs
+++ DapperExtension/MySqlDataAccess.cs
@@ -11,7 +11,46 @@ namespace DapperExtension
         public MySqlDataAccess(string connectionString)
         {
             _connectionString = connectionString;
+            CheckConnection();
         }
+
+        #region Check Connection
+        public bool CheckConnection()
+        {
+            bool isConn = false;
+            MySqlConnection conn = null;
+            try
+            {
+                conn = new MySqlConnection(_connectionString);
+                conn.Open();
+                isConn = true;
+            }
+            catch (ArgumentException a_ex)
+            {
+                Console.WriteLine("Check the Connection String.");
+                Console.WriteLine(a_ex.Message);
+                Console.WriteLine(a_ex.ToString());
+                throw a_ex;
+            }
+            catch (MySqlException ex)
+            {
+                string sqlErrorMessage = "Message: " + ex.Message + "\n" +
+                "Source: " + ex.Source + "\n" +
+                "Number: " + ex.Number;
+                Console.WriteLine(sqlErrorMessage);
+                isConn = false;
+                throw ex;
+            }
+            finally
+            {
+                if (conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+            }
+            return isConn;
+        }
+        #endregion
 
         #region Receive
         public async Task<IEnumerable<T>> LoadDataORM<T, U>(string storedProcedure, U parameters)
